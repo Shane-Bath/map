7fe38c3db97a7bdb41c938103426e040bfe81c29
Shane-Bath <shane.bath@gmail.com>
2023-04-23 14:58:23 +0000

    Add for  of loop example

--- map.js
+++ map.js
@@ -5,11 +5,21 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+};
 
+console.log(results)
 
 // Using map()
+const mulyByTwo = function (num) {
+  return num * 2;
+}
 
-
+const mapResults = nums.map(mulyByTwo);
+console.log(mapResults);
 // Simplified w/ map()
 
 
